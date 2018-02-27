434d844fc2d23b57dd5483e867ff759bc8714907
nvogurtsov <dice2slice@gmail.com>
2018-02-27 17:10:44 +0300

    new file and add some feature

---
+++ functions.py
@@ -0,0 +1,66 @@
+from init import *
+
+
+def generate_params(md):
+    i = 1
+    if parameters:
+        parameters.clear()
+
+    for item in params_list[md]:
+        parameters.append({name: item, priority_name: i})
+        i = i + 1
+    return parameters
+
+
+def generate_tasks(lst, t_list):
+    if tasks:
+        tasks.clear()
+
+#    if i == 1:
+#        tasks_lvl = tasks_list[0]
+#        tasks.append({name: tasks_lvl, parameters_name: lst})
+#        return tasks
+#    elif i == 'full':
+#        tasks_lvl = tasks_list[:]
+#        for task in tasks_lvl:
+#            tasks.append({name: task, parameters_name: lst})
+#        return tasks
+#    elif i == 2:
+#        tasks_lvl = tasks_list[1:len(tasks_list)]
+#        for task in tasks_lvl:
+#            tasks.append({name: task, parameters_name: lst})
+#        return tasks
+#     if len(t_list) >= 2:
+#         tasks_lvl = t_list
+#         for task in tasks_lvl:
+#             tasks.append({name: task, parameters_name: lst})
+#         return tasks
+    if t_list:
+        tasks.append({name: t_list, parameters_name: lst})
+        return tasks
+
+
+def generate_modules(lst, mname):
+    if modules:
+        modules.clear()
+
+    modules.append({name: mname, tasks_name: lst})
+    return modules
+
+
+def generate_agent(lst, branch):
+    if agents:
+        agents.clear()
+
+    agents.append({key_name: agents_list[branch], modules_name: lst})
+    return agents
+
+
+def generate_level(lst, i, l_name):
+    levels.append({name: l_name, priority_name: i, agents_name: lst})
+    return levels
+
+
+def generate_file(lst):
+    return {name: config_name, rules_name: rule, rcaSeverity_name: 5, originatorId_name: originatorId,
+            taskKey_name: taskKey, alertTypeName: alertType, levels_name: lst}
--- init.py
+++ init.py
@@ -1,13 +1,36 @@
+parameters, tasks, modules, agents, levels = [], [], [], [], []
 filename_work = "C:\Tecom\Qligent\RCA\gen\RCA.config"
 filename_home = "C:\RCA.config"
 
-parameters, tasks, modules, agents, levels = [], [], [], [], []
-taskKey = "VLG-NN-CBS-1.SNMPMonitor.VLG-NNOV-MRF SIB.FED.028.SD4.TS1"
-alertTypeName = "qos.RCA.testChanelNN"
-originatorId = 1522072
+agents_list = {"KV": "m7-ql-bks01",
+               "ML": "m7-ql-bks02",
+               "MD": "m7-ql-bks03",
+               "NN": "m7-ql-bks04",
+               "ON": "m7-ql-bks05",
+               "PZ": "m7-ql-bks06",
+               "SM": "m7-ql-bks07",
+               "SR": "m7-ql-bks08",
+               "UD": "m7-ql-bks09",
+               "UL": "m7-ql-bks10",
+               "CH": "m7-ql-bks11",
+               "TT": "m7-ql-bks12"}
 
+modules_list = ["SNMPMonitor"]
+tasks_list = ["VLG-NNOV-MRF SIB.FED.028.SD4.TS1",
+              "VLG-NNOV-RF SZF.FED.032.SD4.0S1", "VLG-NNOV2-MRF SZF.FED.956.HD4.0S1"]   #"MLR", "NoSignal", "BitratePeak"
+params_list = {"SNMPMonitor": ["IAT"],
+               "IPStatistics": ["averageJitter", "mediaLossRate", "mediaLossTotal"],
+               "MpegTSStatisticsIPTVControlModule": ["overallBitrate"],
+               "TR101290": ["Continuity_count_err"]}
+
+
+taskKey = agents_list["NN"] + "." + modules_list[0] + "." + tasks_list[0]
+alertType = "qos.RCA.testChanelNN"
+originatorId = 1522072
+config_name = "Test RCA NN channels"
+rule = ["L2"]
 originatorId_name = "originatorId"
-alertTypeName_name = "alertTypeName"
+alertTypeName = "alertTypeName"
 taskKey_name = "taskKey"
 levels_name = "levels"
 rcaSeverity_name = "rcaSeverity"
@@ -19,12 +42,3 @@ tasks_name = "tasks"
 key_name = "key"
 agents_name = "agents"
 modules_name = "modules"
-
-agents_list = ["m7-ql-bks04"]
-modules_list = ["SNMPMonitor"]
-tasks_list = ["VLG-NNOV-MRF SIB.FED.028.SD4.TS1",
-              "VLG-NNOV-RF SZF.FED.032.SD4.0S1", "VLG-NNOV2-MRF SZF.FED.956.HD4.0S1"]   #"MLR", "NoSignal", "BitratePeak"
-params_list = {"SNMPMonitor": ["IAT"],
-               "IPStatistics": ["averageJitter", "mediaLossRate", "mediaLossTotal"],
-               "MpegTSStatisticsIPTVControlModule": ["overallBitrate"],
-               "TR101290": ["Continuity_count_err"]}

05a0cbdee8ff87b6b4784899cf55fba21c42d81e
nvogurtsov <dice2slice@gmail.com>
2018-03-02 16:25:47 +0300

    add new func

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import copy
 from functions import *
 
 
-def gen_channels(module, branch):
+def make_channel_config(module, branch):
     priority = len(tasks_list)
     for task in tasks_list:
         tmp_data = generate_agent(generate_modules(generate_tasks(generate_params(module), task), module), branch)
@@ -11,7 +11,11 @@ def gen_channels(module, branch):
         priority -= 1
 
 
-gen_channels("SNMPMonitor", "NN")
+def make_probe_config(module, branch):
+    pass
+
+
+make_channel_config("SNMPMonitor", "NN")
 
 data = generate_file(levels)
 
